de1bd76dc446ed9afb7d5792970989f8be0caa36
Vensin <51481323+vxnsin@users.noreply.github.com>
2024-10-16 13:19:09 +0200

    Template Files

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'vxnsin'
+description 'Basic template for a FiveM script'
+version '1.0.0'    
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
